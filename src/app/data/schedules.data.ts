3da178c18b2ee91aec40663a315b17931002cddb
Robert DeSarbo <robertdesarbo@gmail.com>
2021-05-26 07:52:13 -0400

    fix bad time

--- src/app/data/schedules.data.ts
+++ src/app/data/schedules.data.ts
@@ -391,7 +391,7 @@ const schedules: Schedule[] = [
     {
         home: teams.find(element => element.abbreviation == 'CAK') as Team,
         away: teams.find(element => element.abbreviation == 'GIA') as Team,
-        date: new Date('June 13, 2021 1:30:00 PM'),
+        date: new Date('June 13, 2021 3:00:00 PM'),
         field: fields.find(element => element.name == 'Mullens Park') as Field,
         umpires: [Umpire.from({name:'Unknown'})],
     },
