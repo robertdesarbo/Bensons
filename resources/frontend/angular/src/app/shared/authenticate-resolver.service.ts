2c194736b982b84b271d6b918d92900bde863277
Robert DeSarbo <robertdesarbo@gmail.com>
2021-06-09 19:12:43 -0400

    more fix

--- resources/frontend/angular/src/app/shared/authenticate-resolver.service.ts
+++ resources/frontend/angular/src/app/shared/authenticate-resolver.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, Resolve } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
@@ -9,6 +9,7 @@ export class AuthenticationResolver implements Resolve<any> {
 	constructor(private authenticationService: AuthenticationService) { }
 
 	resolve(): Observable<any> {
+		return of(true);
 		// return this.authenticationService.checkAuthentication();
 	}
 }
