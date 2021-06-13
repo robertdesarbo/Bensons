8dfcdc8886cb9fe75145f72af99f1f0eda124bf2
Robert DeSarbo <robertdesarbo@gmail.com>
2021-06-13 19:50:49 -0400

    Fix Nan issue

--- resources/frontend/angular/src/app/models/standings.model.ts
+++ resources/frontend/angular/src/app/models/standings.model.ts
@@ -11,7 +11,11 @@ export class Standings {
 		readonly games_played?: number,
 		readonly win_percentage?: string) {
 		this.games_played = this.won + this.lost;
-		this.win_percentage = (this.won / this.games_played).toFixed(3);
+		if (this.games_played !== 0) {
+			this.win_percentage = (this.won / this.games_played).toFixed(3);
+		} else {
+			this.win_percentage = "0.000";
+		}
 	}
 
 	static from(standings: Standings): Standings {
