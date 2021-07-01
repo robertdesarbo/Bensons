e5d2347561e20aa4dd7c0e4ac498db4aff058204
Robert DeSarbo <robertdesarbo@gmail.com>
2021-07-01 08:38:02 -0400

    Add in twitter feed

--- resources/frontend/angular/src/app/pages/home/home.component.ts
+++ resources/frontend/angular/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Injectable, Inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { DOCUMENT } from '@angular/common';
 
 import { DialogRegisterTeam } from './modals/register-team-dialog.component';
 import { DialogLookingForATeam } from './modals/looking-for-a-team-dialog.component';
 
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { ReplaySubject, Observable, forkJoin } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
 import { LeagueStat } from 'src/app/models/league-stat.model';
@@ -17,18 +18,19 @@ import { LeagueStat } from 'src/app/models/league-stat.model';
 	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements AfterViewInit {
+	private _loadedLibraries: { [url: string]: ReplaySubject<any> } = {};
 	stacked = false;
 
 	public leagueStat$: Observable<LeagueStat[]>;
 
-	constructor(public dialog: MatDialog, public http: HttpClient) {
+	constructor(public dialog: MatDialog, public http: HttpClient, @Inject(DOCUMENT) private readonly document: any) {
 		this.leagueStat$ = this.http.get<LeagueStat[]>('/api/home/stats').pipe(
 			shareReplay()
 		);
 	}
 
 	ngAfterViewInit(): void {
-		(<any>window).twttr.widgets.load();
+		this.document.defaultView.twttr.widgets.load();
 	}
 
 	openRegisterDialog(): void {
