ef8c046027eedc9f59dc16a81327ac09bbf922e1
Robert DeSarbo <robertdesarbo@gmail.com>
2021-08-01 16:02:21 -0400

    updates to managing league page

--- resources/frontend/angular/src/app/pages/manage-league/manage-league.component.ts
+++ resources/frontend/angular/src/app/pages/manage-league/manage-league.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { Team } from 'src/app/models/team.model';
 import { League } from 'src/app/models/league.model';
@@ -24,31 +25,69 @@ export class ManageLeagueComponent {
 	public league$: Observable<League[]>;
 
 	public sports = Object.values(Sport);
+	public leagues: League[];
+	public divisions: Division[];
+
+	public step = 0;
 
 	constructor(private formBuilder: FormBuilder,
 		private http: HttpClient) {
 
 		// pull in data
 		this.league$ = this.http.get<League[]>('/api/league');
+		this.league$.pipe(
+			tap((leagues) => {
+				this.leagues = leagues;
+			})
+		).subscribe();
 
 		this.leagueFormGroup = this.formBuilder.group({
 			league: '',
+			name: '',
 			sport: ''
 		});
 
-		this.leagueFormGroup.get("league").valueChanges.subscribe(league => {
+		this.divisionFormGroup = this.formBuilder.group({
+			division: '',
+			name: '',
+		});
+
+		this.leagueFormGroup.get("league").valueChanges.subscribe(league_id => {
 			const option = {
 				params: {
-					league: league
+					league: league_id
 				}
 			}
 
-			// this.leagueFormGroup.get('division').setValue(null);
+			const league = this.leagues.find(league => league.id === league_id);
+			this.leagueFormGroup.get('name').setValue(league.name);
+			this.leagueFormGroup.get('sport').setValue(league.sport);
+
 			this.division$ = this.http.get<Division[]>('/api/division-by-league', option);
+			this.division$.pipe(
+				tap((divisions) => {
+					this.divisions = divisions;
+				})
+			).subscribe();
+		});
+
+		this.divisionFormGroup.get("division").valueChanges.subscribe(division_id => {
+			const division = this.divisions.find(division => division.id === division_id);
+
+			this.divisionFormGroup.get('name').setValue(division.name);
 		});
 	}
 
-	public updateLeague() {
 
+	setStep(index: number) {
+		this.step = index;
+	}
+
+	nextStep() {
+		this.step++;
+	}
+
+	prevStep() {
+		this.step--;
 	}
 }
