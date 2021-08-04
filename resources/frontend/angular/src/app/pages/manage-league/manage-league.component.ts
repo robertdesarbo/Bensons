f4afea864a4937f2d8bba05f9fd5e7c98a285af3
Robert DeSarbo <robertdesarbo@gmail.com>
2021-08-04 09:20:40 -0400

    update styling to league management page

--- resources/frontend/angular/src/app/pages/manage-league/manage-league.component.ts
+++ resources/frontend/angular/src/app/pages/manage-league/manage-league.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Observable, of } from 'rxjs';
-import { tap, shareReplay } from 'rxjs/operators';
+import { tap, shareReplay, filter } from 'rxjs/operators';
 
 import { League } from 'src/app/models/league.model';
 import { Division } from 'src/app/models/division.model';
@@ -36,6 +36,8 @@ export class ManageLeagueComponent {
 
 	public step = 0;
 
+	public addLeague = false;
+
 	constructor(private formBuilder: FormBuilder,
 		private http: HttpClient) {
 
@@ -67,12 +69,21 @@ export class ManageLeagueComponent {
 			offical_fee_per_game: '',
 		});
 
+		this.teamFormGroup = this.formBuilder.group({
+			team: ''
+		});
+
 		this.leagueFormGroup.get("league").valueChanges.subscribe(league_id => {
 			const option = {
 				params: {
 					league: league_id
 				}
 			}
+
+			// reset other forms
+			this.divisionFormGroup.get("division").setValue(null);
+			this.seasonFormGroup.get("season").setValue(null);
+			this.teamFormGroup.get("team").setValue(null);
 
 			const league = this.leagues.find(league => league.id === league_id);
 			this.leagueFormGroup.get('name').setValue(league.name);
@@ -87,36 +98,50 @@ export class ManageLeagueComponent {
 			).subscribe();
 		});
 
-		this.divisionFormGroup.get("division").valueChanges.subscribe(division_id => {
-			const option = {
-				params: {
-					division: division_id
+		this.divisionFormGroup.get("division").valueChanges
+			.pipe(filter(division_id => division_id !== null))
+			.subscribe(division_id => {
+				const option = {
+					params: {
+						division: division_id
+					}
 				}
-			}
 
-			const division = this.divisions.find(division => division.id === division_id);
-			this.divisionFormGroup.get('name').setValue(division.name);
+				// reset other forms
+				this.seasonFormGroup.get("season").setValue(null);
+				this.teamFormGroup.get("team").setValue(null);
 
-			this.season$ = this.http.get<Season[]>('/api/season-by-division', option);
-			this.season$.pipe(
-				shareReplay(),
-				tap((divisions) => {
-					this.seasons = divisions;
-				})
-			).subscribe();
-		});
+				const division = this.divisions.find(division => division.id === division_id);
+				this.divisionFormGroup.get('name').setValue(division.name);
 
-		this.seasonFormGroup.get("season").valueChanges.subscribe(season_id => {
+				this.season$ = this.http.get<Season[]>('/api/season-by-division', option);
+				this.season$.pipe(
+					shareReplay(),
+					tap((divisions) => {
+						this.seasons = divisions;
+					})
+				).subscribe();
+			});
 
-			const season = this.seasons.find(season => season.id === season_id);
+		this.seasonFormGroup.get("season").valueChanges
+			.pipe(filter(season_id => season_id !== null))
+			.subscribe(season_id => {
 
-			this.seasonFormGroup.get('start_at').setValue(season.start_at);
-			this.seasonFormGroup.get('number_of_games').setValue(season.number_of_games);
-			this.seasonFormGroup.get('league_fee').setValue(season.league_fee);
-			this.seasonFormGroup.get('offical_fee_per_game').setValue(season.offical_fee_per_game);
-		});
+				// reset other forms
+				this.teamFormGroup.get("team").setValue(null);
+
+				const season = this.seasons.find(season => season.id === season_id);
+
+				this.seasonFormGroup.get('start_at').setValue(season.start_at);
+				this.seasonFormGroup.get('number_of_games').setValue(season.number_of_games);
+				this.seasonFormGroup.get('league_fee').setValue(season.league_fee);
+				this.seasonFormGroup.get('offical_fee_per_game').setValue(season.offical_fee_per_game);
+			});
 	}
 
+	toggleLeagueFormType() {
+		this.addLeague = !this.addLeague;
+	}
 
 	setStep(index: number) {
 		this.step = index;
