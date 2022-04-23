46f7f86ccb77ad40c9de25c031bec9bb5ccca9a0
Robert DeSarbo <robertdesarbo@gmail.com>
2022-04-23 16:19:02 -0400

    Updated Rules & Sponsors Page Improvements

--- resources/frontend/angular/src/app/pages/sponsors/sponsors.component.ts
+++ resources/frontend/angular/src/app/pages/sponsors/sponsors.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import 'keen-slider/keen-slider.min.css';
-import KeenSlider from 'keen-slider';
+import {AfterViewInit, OnDestroy, Component, ElementRef, ViewChild} from '@angular/core';
+import KeenSlider, { KeenSliderInstance } from 'keen-slider';
 
 @Component({
 	selector: 'app-sponsors',
 	templateUrl: './sponsors.component.html',
-	styleUrls: ['./sponsors.component.scss']
+	styleUrls: [
+    '../../../../node_modules/keen-slider/keen-slider.min.css',
+    './sponsors.component.scss'
+  ]
 })
-export class SponsorsComponent implements OnInit {
+export class SponsorsComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('sliderRef') sliderRef: ElementRef<HTMLElement>;
 
-	constructor() { }
+  slider: KeenSliderInstance = null;
+  currentSlide: number = 1
 
-	ngOnInit(): void {
-		let slider = new KeenSlider(
-			'#sponsor-slider',
-			{
-				loop: true,
-				created: () => {
-					console.log('created')
-				},
-			},
-			[
-				// add plugins here
-			]
-		)
-	}
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      loop: true,
+      breakpoints: {
+        "(min-width: 400px)": {
+          slides: { perView: 5, spacing: 5 },
+        },
+        "(min-width: 1000px)": {
+          slides: { perView: 8, spacing: 10 },
+        },
+      },
+      slides: { perView: 1 },
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.slider) { this.slider.destroy(); }
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,47 +14,28 @@ use App\Http\Controllers\AngularController;
 |
 */
 
-Route::get('download-sponsor/{sponsor}', function ($sponsor) {
-    if ($sponsor == "Contemporary-Designs") {
-        $file_name = "Contemporary-Designs-star.jpg";
-    } elseif ($sponsor == "Pella") {
-        $file_name = "Pella.png";
-    }  elseif ($sponsor == "Gettysburg-Flag-Works") {
-        $file_name = "Gettysburg-Flag-Works.png";
-    } elseif ($sponsor == "Wyatts-Wicked-Goods") {
-        $file_name = "Wyatts-Wicked-Goods.png";
-    } elseif ($sponsor == "Spinners-Pizza") {
-        $file_name = "Spinners-Pizza.jpeg";
-    }
-
-    return response()->file("build/assets/content/sponsors/".$file_name);
-});
-
 Route::get('download/{rule}', function ($rule) {
-    if ($rule == "Benson_Sunday") {
-        $file_name = "Empire-State-Sports-Sunday-League-Rules-2021.pdf";
-    } elseif ($rule == "Lynn_COED") {
-        $file_name = "Rules-LYN-Coed-2021.pdf";
-    } elseif ($rule == "Basketball_Sponsor_Letter") {
-        $file_name = "Sponsor-Letter-BESS-BBall-2021-22.doc";
-    } elseif ($rule == "ASA_Not_Approved") {
-        $file_name = "ASA-Non-Approved-BatList-1Page.pdf";
-    } elseif ($rule == "Basketball_Info_Sheet") {
-        $file_name = "Basketball-Information-Sheet-2021.jpg";
-    } elseif ($rule == "Basketball_Sign_Up_Sheet") {
-        $file_name = "Basketball-Sign-Up-Sheet-2021.jpg";
-    } elseif ($rule == "Basketball_Rules") {
-        $file_name = "Rules-Bensons-Basketball-2021.pdf";
+
+    if ($rule == "ASA_Not_Approved") {
+        $file_name = "2022/2022-ASA-Non-Approved-BatList.pdf";
+    } else if ($rule == "Ground_Rules") {
+        $file_name = "2022/2022-Ground-Rules.pdf";
+    } else if ($rule == "Roster_Sheet") {
+        $file_name = "2022/2022-Roster-Sheet.xlsx";
+    } else if ($rule == "2022_BESS_Softball_COED_Rules") {
+        $file_name = "2022/bensons-softball/2022-BESS-Softball-Coed-Rules.pdf";
     } elseif ($rule == "2022_BESS_Softball_Info_Sheet_And_Signup_Sheet") {
         $file_name = "2022/bensons-softball/2022-BESS-Softball-Info-Sheet-And-Signup-Sheet.pdf";
     } elseif ($rule == "2022_BESS_Softball_Sponsor_Letter") {
         $file_name = "2022/bensons-softball/2022-BESS-Softball-Sponsor-Letter.pdf";
+    } else if ($rule == "2022_Lynns_Softball_COED_Rules") {
+        $file_name = "2022/lynns-softball/2022-Lynns-Softball-Coed-Rules.pdf";
+    } else if ($rule == "2022_Lynns_Softball_Mens_Rules") {
+        $file_name = "2022/lynns-softball/2022-Lynns-Softball-Mens-Rules.pdf";
     } elseif ($rule == "2022_Lynns_Softball_Info_Sheet_And_Signup_Sheet") {
         $file_name = "2022/lynns-softball/2022-Lynns-Softball-Info-Sheet-And-Signup-Sheet.pdf";
     } elseif ($rule == "2022_Lynns_Softball_Sponsor_Letter") {
         $file_name = "2022/lynns-softball/2022-Lynns-Softball-Sponsor-Letter.pdf";
-    } else {
-        $file_name = "Rules-LYN-Mens-2021.pdf";
     }
 
     return response()->download("build/assets/content/".$file_name);
