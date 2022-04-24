5977954daffa410b23eb8b52caf416aa5cb04445
Robert DeSarbo <robertdesarbo@gmail.com>
2022-04-24 11:53:02 -0400

    Fix Sponsors Page

--- resources/frontend/angular/src/app/pages/sponsors/sponsors.component.ts
+++ resources/frontend/angular/src/app/pages/sponsors/sponsors.component.ts
@@ -13,20 +13,20 @@ export class SponsorsComponent implements AfterViewInit, OnDestroy {
   @ViewChild('sliderRef') sliderRef: ElementRef<HTMLElement>;
 
   slider: KeenSliderInstance = null;
-  currentSlide: number = 1
+  currentSlide = 1;
 
   ngAfterViewInit() {
     this.slider = new KeenSlider(this.sliderRef.nativeElement, {
       loop: true,
+      slides: { perView: 1 },
       breakpoints: {
         "(min-width: 400px)": {
-          slides: { perView: 5, spacing: 5 },
+          slides: { perView: 4, spacing: 5 },
         },
-        "(min-width: 1000px)": {
+        "(min-width: 800px)": {
           slides: { perView: 8, spacing: 10 },
         },
       },
-      slides: { perView: 1 },
     });
   }
 
