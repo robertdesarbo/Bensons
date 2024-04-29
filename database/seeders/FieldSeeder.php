00048289adfb4e70d527642ba2a5b607971d37d0
Shift <shift@laravelshift.com>
2024-04-29 16:18:19 +0000

    Remove redundant typing from DocBlocks

--- database/seeders/FieldSeeder.php
+++ database/seeders/FieldSeeder.php
@@ -11,8 +11,6 @@ class FieldSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
     public function run(): void
     {
--- database/seeders/ScheduleSeeder.php
+++ database/seeders/ScheduleSeeder.php
@@ -9,8 +9,6 @@ class ScheduleSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
     public function run(): void
     {
